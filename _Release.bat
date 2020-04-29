19f544e867e24b91759c8b282da9f0669ce107c0
stackprobe <stackprobes@gmail.com>
2020-04-30 06:37:43 +0900

    Flora

--- _Release.bat
+++ _Release.bat
@@ -6,11 +6,10 @@ C:\Factory\SubTools\resCluster.exe /L Music.txt       /D C:\tmp /K %RAWKEY% /C o
 C:\Factory\SubTools\resCluster.exe /L Picture.txt     /D C:\tmp /K %RAWKEY% /C out\Data2.dat
 C:\Factory\SubTools\resCluster.exe /L SoundEffect.txt /D C:\tmp /K %RAWKEY% /C out\Data3.dat
 
-COPY RamenX\Release\RamenX.exe out\しろくまACT.exe
-rem COPY IMenu\IMenu\bin\Release\IMenu.exe "out\しろくまACT スタート.exe"
+COPY /B RamenX\Release\RamenX.exe out\しろくまACT.exe
+COPY /B AUTHORS out
 C:\Factory\Tools\xcp.exe doc out
 ren out\Manual.txt マニュアル.txt
-ren out\Changelog.txt 更新履歴.txt
 
 C:\Factory\SubTools\zip.exe /G out ShirokumaACT
 C:\Factory\Tools\summd5.exe /M out
